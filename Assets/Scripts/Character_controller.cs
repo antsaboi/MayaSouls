b372094e0d039eb921b48d2bc519c3d9bb37ed61
antsaboi <anton.lehtola@hotmail.com>
2021-02-10 19:22:53 +0200

    Revert "Revert "Unity ver update to 2020.2.1f1 and animations and movement stuffs""
    This reverts commit cfe5d9915d8343b1e1339438db6eef950f1aff40.

--- Assets/Scripts/Character_controller.cs
+++ Assets/Scripts/Character_controller.cs
@@ -26,9 +26,9 @@ public class Character_controller : MonoBehaviour
 
     //Buttons
 
-    bool buttonRight; 
+/*    bool buttonRight; 
     bool buttonLeft;
-    bool buttonJump;
+    bool buttonJump;*/
     
     //Movement forces
 
@@ -39,114 +39,137 @@ public class Character_controller : MonoBehaviour
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
+    /*    // Update is called once per frame
+
+        private void Update()
+        {
+            GetInput();
+            if (state == "IDLE") StateIdle();
+            if (state == "RUN") StateRun();
+            if (state == "JUMP") StateJump();
+        }
+
+        private void FixedUpdate()
+        {
+            if (buttonRight)
+            {
+                body.AddForce(Vector2.right * moveSpeed);
+            }
+            if (buttonJump)
+            {
+                body.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            }
+            if (buttonLeft)
+            {
+                body.AddForce(Vector2.left * moveSpeed);
+            }
+        }
+
+        void StateJump()
+        {
+            Debug.Log("State is jump");
+            if (body.velocity.y == 0) // Trigger Idle -> Jump
+            {
+                state = "IDLE";
+                animator.SetBool("Jump", false);
+                animator.SetBool("Idle", true);
+            }
+        }
+
+        void StateIdle()
+        {
+            Debug.Log("State is idle");
+            if (buttonRight) // Trigger Idle -> Run
+            {
+                state = "RUN";
+                animator.SetBool("Run", true);
+                animator.SetBool("Idle", false);
+            }
+            if (buttonJump) // Trigger Idle -> Jump
+            {
+                state = "JUMP";
+                animator.SetBool("Jump", true);
+                animator.SetBool("Idle", false);
+            }
+        }
+
+        void StateRun()
+        {
+            Debug.Log("State is run");
+            if (!buttonRight)
+            {
+                state = "IDLE";
+                animator.SetBool("Run", false);
+                animator.SetBool("Idle", true);
+            }
+        }
+
+        void GetInput()
+        {
+            buttonRight = Input.GetKey("d");
+            buttonJump = Input.GetKey("w");
+            buttonLeft = Input.GetKey("a");
+        }*/
 
     private void Update()
     {
-        GetInput();
-        if (state == "IDLE") StateIdle();
-        if (state == "RUN") StateRun();
-        if (state == "JUMP") StateJump();
-    }
+        float xMovement = Input.GetAxisRaw("Horizontal");
 
-    private void FixedUpdate()
-    {
-        if (buttonRight)
-        {
-            body.AddForce(Vector2.right * moveSpeed);
-        }
-        if (buttonJump)
-        {
-            body.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
-        }
-        if (buttonLeft)
-        {
-            body.AddForce(Vector2.left * moveSpeed);
-        }
-    }
-
-    void StateJump()
-    {
-        Debug.Log("State is jump");
-        if (body.velocity.y == 0) // Trigger Idle -> Jump
-        {
-            state = "IDLE";
-            animator.SetBool("Jump", false);
-            animator.SetBool("Idle", true);
-        }
-    }
-
-    void StateIdle()
-    {
-        Debug.Log("State is idle");
-        if (buttonRight) // Trigger Idle -> Run
-        {
-            state = "RUN";
-            animator.SetBool("Run", true);
-            animator.SetBool("Idle", false);
-        }
-        if (buttonJump) // Trigger Idle -> Jump
-        {
-            state = "JUMP";
-            animator.SetBool("Jump", true);
-            animator.SetBool("Idle", false);
-        }
-    }
-
-    void StateRun()
-    {
-        Debug.Log("State is run");
-        if (!buttonRight)
-        {
-            state = "IDLE";
-            animator.SetBool("Run", false);
-            animator.SetBool("Idle", true);
-        }
-    }
-
-    void GetInput()
-    {
-        buttonRight = Input.GetKey("d");
-        buttonJump = Input.GetKey("w");
-        buttonLeft = Input.GetKey("a");
-    }
-
-    void VanhaUpdate()
-    {
-        float xMovement = Input.GetAxis("Horizontal");
-
-        animator.SetFloat("Speed", xMovement);
+        //animator.SetFloat("Speed", xMovement);
 
         currentVelocity = new Vector2(movingSpeed * xMovement, body.velocity.y);
 
+        if (Mathf.Abs(currentVelocity.x) > 3.5f)
+        {
+            animator.SetBool("Run", true);
+        }
+        else {
+            animator.SetBool("Run", false);
+        }
+
+        animator.SetFloat("YSpeed", currentVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            currentVelocity.y = jumpSpeed;
+        }
+
+
+        if (xMovement > 0.1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (xMovement < -0.1f) {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        body.velocity = currentVelocity;
+    }
+
+    void FixedUpdate()
+    {
         Debug.DrawRay(transform.position, new Vector2(0, -raycastDistance), Color.white, 01f);
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, raycastDistance, groundMask);
 
         // If it hits something...
         if (hit.collider != null)
-
         {
-            if(hit.collider.gameObject.tag == "Ground")
+            if (hit.collider.gameObject.CompareTag("Ground"))
             {
                 isGrounded = true;
+                animator.SetBool("InAir", false);
             }
- 
-        } else
+
+        }
+        else
         {
             isGrounded = false;
+            animator.SetBool("InAir", true);
         }
-
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
-        {
-            currentVelocity.y = jumpSpeed;
-            /*isGrounded = false;*/
-        }
-
-      
-            body.velocity = currentVelocity;
     }
 
     //Sillä hetkellä kun osutaan toiseen collideriin
